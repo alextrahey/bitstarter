37633c52fe098a87a31419fae34c491c47e79eb3
alextrahey <alex.trahey@gmail.com>
2013-07-26 18:55:09 +0000

    bug fix

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
@@ -10,3 +12,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
